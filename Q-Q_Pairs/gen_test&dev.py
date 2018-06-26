c57e3e7b4b2cca83204334ef8c0762ed2255d669
Runchen <remexhu@icloud.com>
2018-06-26 11:53:34 +0800

    randomly extract labeled queries

--- Q-Q_Pairs/gen_test&dev.py
+++ Q-Q_Pairs/gen_test&dev.py
@@ -1,5 +1,6 @@
 from tqdm import tqdm
 import ujson as json
+import random
 
 def get_number_of_lines(file_path):
     count = 0
@@ -9,17 +10,24 @@ def get_number_of_lines(file_path):
     return count
 
 
-def gen_test(input_file, output_file=None):
+def gen_test(input_file, output_file=None, other_file=None):
     if not output_file:
-        output_file = 'result_labeled.txt'
+        output_file = 'result_labeled'
+    if not other_file:
+        other_file = 'result_other'
 
-    sampled_cnt = 10
+
+
     sampled_lines = []
+    other_lines = []
 
     line_cnt = 0
     num_lines = get_number_of_lines(input_file)
 
-    
+    choice = random.sample(range(num_lines), 1000)
+
+    query_count, candidate_count, selected_count, unselected_count = 0, 0, 0, 0
+
     invalid_chars = set(['。', '：', '-', '，'])
     with open(input_file, "r", encoding='utf-8') as reader:
         for line in tqdm(reader, total=num_lines, ascii=True):
@@ -28,30 +36,51 @@ def gen_test(input_file, output_file=None):
         #     sample is a dict
             except:
                 continue
-
+            line_cnt += 1
 
             documents = sample['documents']
             question = sample['question']
+            query_count += 1
             title_list = {}
 
-            for document in documents:
-                title = document['title']
-                if document['is_selected']:
-                    label = 1
-                else:
-                    label = 0
-                # label = document['most_related_para']
-                title_list[title] = label
+            if line_cnt in choice:
+                for document in documents:
+                    candidate_count += 1
+                    title = document['title']
+                    if document['is_selected']:
+                        selected_count += 1
+                        label = 1
+                    else:
+                        unselected_count += 1
+                        label = 0
+                    # label = document['most_related_para']
+                    title_list[title] = label
 
-            # print(title_list)
-            if title_list:
-                for title, label in title_list.items():
-                    sample_line = question + '\t' + title + '\t' + str(label) +'\n'
-                    sampled_lines.append(sample_line)
+                # print(title_list)
+                if title_list:
+                    for title, label in title_list.items():
+                        sample_line = question + '\t' + title + '\t' + str(label) +'\n'
+                        sampled_lines.append(sample_line)
+            else:
+                title_list = set()
+
+                for document in documents:
+                    if document['is_selected']:
+                        title_list.add(document['title'])
+
+                # print(title_list)
+                if title_list:
+                    for title in title_list:
+                        sample_line = question + '\t' + title + '\n'
+                        other_lines.append(sample_line)
+
+    # print(query_count, candidate_count / query_count, selected_count / query_count, unselected_count / query_count)
 
     # print(len(sampled_lines))
     with open(output_file, "w", encoding='utf-8') as writer:
         writer.writelines(sampled_lines)
+    with open(other_file, "w", encoding='utf-8') as writer:
+        writer.writelines(other_lines)
 
 
 gen_test('zhidao.dev.json')
