6e4f7370c1320c27f6e4865124372ff0df6f8a32
Runchen <remexhu@icloud.com>
2018-06-26 17:44:38 +0800

    split into dev/test/train files

--- Q-Q_Pairs/gen_test&dev.py
+++ Q-Q_Pairs/gen_test&dev.py
@@ -10,30 +10,32 @@ def get_number_of_lines(file_path):
     return count
 
 
-def gen_test(input_file, output_file=None, other_file=None):
-    if not output_file:
-        output_file = 'result_labeled'
+def gen_test(input_file, output_file_dev=None, output_file_tst=None, other_file=None):
+    if not output_file_dev:
+        output_file_dev = 'result_dev.json'
     if not other_file:
-        other_file = 'result_other'
+        other_file = 'result_other.json'
+    if not output_file_tst:
+        output_file_tst = 'result_tst.json'
 
 
-
-    sampled_lines = []
+    dev_lines = []
+    tst_lines = []
     other_lines = []
 
     line_cnt = 0
     num_lines = get_number_of_lines(input_file)
 
     choice = random.sample(range(num_lines), 1000)
+    choice_dev = choice[:500]
+    choice_tst = choice[:-1]
 
     query_count, candidate_count, selected_count, unselected_count = 0, 0, 0, 0
 
-    invalid_chars = set(['。', '：', '-', '，'])
     with open(input_file, "r", encoding='utf-8') as reader:
         for line in tqdm(reader, total=num_lines, ascii=True):
             try:
                 sample = json.loads(line)
-        #     sample is a dict
             except:
                 continue
             line_cnt += 1
@@ -43,7 +45,7 @@ def gen_test(input_file, output_file=None, other_file=None):
             query_count += 1
             title_list = {}
 
-            if line_cnt in choice:
+            if line_cnt in choice_dev:
                 for document in documents:
                     candidate_count += 1
                     title = document['title']
@@ -60,7 +62,29 @@ def gen_test(input_file, output_file=None, other_file=None):
                 if title_list:
                     for title, label in title_list.items():
                         sample_line = question + '\t' + title + '\t' + str(label) +'\n'
-                        sampled_lines.append(sample_line)
+                        dev_lines.append(sample_line)
+
+            elif line_cnt in choice_tst:
+                for document in documents:
+                    candidate_count += 1
+                    title = document['title']
+                    if document['is_selected']:
+                        selected_count += 1
+                        label = 1
+                    else:
+                        unselected_count += 1
+                        label = 0
+                    # label = document['most_related_para']
+                    title_list[title] = label
+
+                # print(title_list)
+                if title_list:
+                    for title, label in title_list.items():
+                        sample_line = question + '\t' + title + '\t' + str(label) +'\n'
+                        tst_lines.append(sample_line)
+
+
+
             else:
                 title_list = set()
 
@@ -76,9 +100,11 @@ def gen_test(input_file, output_file=None, other_file=None):
 
     # print(query_count, candidate_count / query_count, selected_count / query_count, unselected_count / query_count)
 
-    # print(len(sampled_lines))
-    with open(output_file, "w", encoding='utf-8') as writer:
-        writer.writelines(sampled_lines)
+    # print(len(dev_lines))
+    with open(output_file_dev, "w", encoding='utf-8') as writer:
+        writer.writelines(dev_lines)
+    with open(output_file_tst, "w", encoding='utf-8') as writer:
+        writer.writelines(tst_lines)
     with open(other_file, "w", encoding='utf-8') as writer:
         writer.writelines(other_lines)
 
